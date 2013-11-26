ec10432f2c0b0aa0e1a6be3a07c6468d5bb185ad
Goran <gorancolovic10@gmail.com>
2013-11-26 11:51:39 +0100

    initial commit

---
+++ src/gitproject/Print.java
@@ -0,0 +1,18 @@
+package gitproject;
+
+class Sent{
+	
+	public Sent(){
+		
+		System.out.println("Hello again");
+
+	}
+}
+
+public class Print {
+
+	public static void main(String[] args){
+		
+		new Sent();
+	}
+}
